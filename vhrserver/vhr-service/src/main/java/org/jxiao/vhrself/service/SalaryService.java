486b505b88924c44c3da58ddd7a312e3470ae761
HerrBaumx <731278859@qq.com>
2020-07-19 23:24:35 +0800

    工资套账添加服务端接口设计

--- vhrserver/vhr-service/src/main/java/org/jxiao/vhrself/service/SalaryService.java
+++ vhrserver/vhr-service/src/main/java/org/jxiao/vhrself/service/SalaryService.java
@@ -5,6 +5,7 @@ import org.jxiao.vhrself.model.Salary;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -16,4 +17,10 @@ public class SalaryService {
     public List<Salary> getAllSalaries() {
         return salaryMapper.getAllSalaries();
     }
+
+    public Integer addSalary(Salary salary) {
+        salary.setCreateDate(new Date());
+        return salaryMapper.insertSelective(salary);
+    }
+
 }
--- vhrserver/vhr-web/src/main/java/org/jxiao/vhrself/controller/salary/SalaryController.java
+++ vhrserver/vhr-web/src/main/java/org/jxiao/vhrself/controller/salary/SalaryController.java
@@ -1,11 +1,10 @@
 package org.jxiao.vhrself.controller.salary;
 
+import org.jxiao.vhrself.model.RespBean;
 import org.jxiao.vhrself.model.Salary;
 import org.jxiao.vhrself.service.SalaryService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,5 +17,13 @@ public class SalaryController {
     @GetMapping("/")
     public List<Salary> getAllSalaries() {
         return salaryService.getAllSalaries();
+    }
+
+    @PostMapping("/")
+    public RespBean addSalary(@RequestBody Salary salary) {
+        if (salaryService.addSalary(salary)==1) {
+            return RespBean.ok("添加成功！");
+        }
+        return RespBean.error("添加失败!");
     }
 }
