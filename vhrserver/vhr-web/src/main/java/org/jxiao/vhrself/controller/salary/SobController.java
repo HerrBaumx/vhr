75f71e1988357848f152a8e6c9a361b10abb5aba
HerrBaumx <731278859@qq.com>
2020-07-20 22:54:06 +0800

    员工账套调整页面设计

--- vhrserver/vhr-web/src/main/java/org/jxiao/vhrself/controller/salary/SobController.java
+++ vhrserver/vhr-web/src/main/java/org/jxiao/vhrself/controller/salary/SobController.java
@@ -2,12 +2,16 @@ package org.jxiao.vhrself.controller.salary;
 
 import org.jxiao.vhrself.model.RespBean;
 import org.jxiao.vhrself.model.RespPageBean;
+import org.jxiao.vhrself.model.Salary;
 import org.jxiao.vhrself.service.EmployeeService;
+import org.jxiao.vhrself.service.SalaryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/salary/sobcfg")
@@ -16,11 +20,19 @@ public class SobController {
     @Autowired
     EmployeeService employeeService;
 
+    @Autowired
+    SalaryService salaryService;
+
     @GetMapping("/")
     public RespPageBean getEmployeeByPageWithSalary(@RequestParam(defaultValue = "1") Integer page,
                                                     @RequestParam(defaultValue = "10") Integer size) {
 
         return employeeService.getEmployeeByPageWithSalary(page, size);
 
+    }
+
+    @GetMapping("/salaries")
+    public List<Salary> getAllSalaries() {
+        return salaryService.getAllSalaries();
     }
 }
