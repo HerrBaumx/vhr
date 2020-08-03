6d474c63a722e04d79045137598679914c7f6b4e
HerrBaumx <731278859@qq.com>
2020-08-03 21:55:06 +0800

    个人中心数据修改

--- vhrserver/vhr-web/src/main/java/org/jxiao/vhrself/HrInfoController.java
+++ vhrserver/vhr-web/src/main/java/org/jxiao/vhrself/HrInfoController.java
@@ -1,14 +1,34 @@
 package org.jxiao.vhrself;
 
 import org.jxiao.vhrself.model.Hr;
+import org.jxiao.vhrself.model.RespBean;
+import org.jxiao.vhrself.service.HrSerice;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HrInfoController {
+    @Autowired
+    HrSerice hrSerice;
+
     @GetMapping("/hr/info")
     public Hr getCurrentHr(Authentication authentication) {
         return ((Hr) authentication.getPrincipal());
+    }
+
+    @PutMapping("/hr/info")
+    public RespBean updateHr(@RequestBody Hr hr, Authentication authentication) {
+        if (hrSerice.updateHr(hr) == 1) {
+            SecurityContextHolder.getContext().setAuthentication(new UsernamePasswordAuthenticationToken(hr,
+                    authentication.getCredentials(), authentication.getAuthorities()));
+            return RespBean.ok("更新成功！");
+        }
+        return RespBean.error("更新失败！");
     }
 }
